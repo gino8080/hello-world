7abaebffec10464613b0622a15527d655b4238fa
Magico80 <magie.cannizzaro@gmail.com>
2017-12-28 19:05:29 +0100

    ngFor and TrackBy - optimize DOM partial render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  recipes = [{
+  recipes;
+
+  loadRecipes(){
+    this.recipes=[
+      {
     id:1,title:"patate riso e cozze"
-  },
-  {
-    id:1,title:"carbonara"
-  },
-    {id:1,title:"pasta al pesto"}
-  ];
-
-  onAdd(){
-    this.recipes.push({
-      id:4,
-      title:"new recipe"
-    })
+    },
+    {
+      id:1,title:"carbonara"
+    },
+      {id:1,title:"pasta al pesto"}
+    ];
   }
 
-  onRemove(recipe){
-    let index = this.recipes.indexOf(recipe)
-    this.recipes.splice(index,1);
-  }
-
-  onChange(recipe){
-   recipe.title="Changed!"
+  trackCourse(index, recipe){
+    recipe ? recipe.id : undefined; 
   }
 
 }
