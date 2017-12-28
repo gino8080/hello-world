840b60bd5046b318d1a520b5093734fa750028b2
Magico80 <magie.cannizzaro@gmail.com>
2017-12-28 12:22:08 +0100

    base component

--- src/app/recipes.component.ts
+++ src/app/recipes.component.ts
@@ -3,10 +3,25 @@ import {Component} from '@angular/core'
 //decorator function
 @Component({
     selector:'recipes', //<recipes> //css selector could be <element>/.class/#id
-    template : '<h2>Recipes</h2>'
+    //template : '<h2>{{ "Title :"+title }}</h2>' //databinding 
+    template : `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let recipe of recipes">
+                {{recipe}}
+            </li> 
+        </ul>
+    ` //string interpolation 
+    //* when the DOM should be modified
 })
 //Pascal Naming convention
 export class RecipesComponent{
     //to make an Angular Component we add DECORATORS
+    title= "Lista di Ricette";
 
+    recipes = ["Riso Patate e cozze","Carbonara","Abbacchio"]
+
+    getTitle(){
+        return this.title;
+    }
 }
