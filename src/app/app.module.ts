21423739f9a1b2e302bc16a74f320c4aa532bd17
Magico80 <magie.cannizzaro@gmail.com>
2018-01-02 11:53:13 +0100

    ReactiveFormsModule 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes.component';
@@ -37,7 +37,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
   //Array of need module
   imports: [
     BrowserModule, //needed for every browser app
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule //needed for Reactive form
   ],
   //List of all Dependencies used inthe components
   //Singleton Pattern
