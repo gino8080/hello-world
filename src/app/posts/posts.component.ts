818e778fa24db5f71e0a8d557f80ef121ff23e67
Magico80 <magie.cannizzaro@gmail.com>
2018-01-02 17:04:18 +0100

    Create DATA

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,11 +8,29 @@ import { Http } from '@angular/http';
 })
 export class PostsComponent implements OnInit {
 
-  posts :any;
-  constructor(http:Http){
-    http.get("https://jsonplaceholder.typicode.com/posts").subscribe((response)=>{
+  posts :any[];
+  private url = "https://jsonplaceholder.typicode.com/posts";
+
+  //private so is accessible also in other methods of the class
+  constructor(private http:Http){
+    http.get(this.url).subscribe((response)=>{
       this.posts = response.json();
     })
+  }
+
+  createPost(input: HTMLInputElement){
+    
+    let post = {
+      title: input.value
+      //description ... other values
+    }
+    input.value = '';
+    this.http.post(this.url,JSON.stringify(post))
+      .subscribe(response=>{
+        post['id'] = response.json().id; //['id'] to avoid ts error 
+        this.posts.splice(0,0,post)
+        console.log(response.json())
+      })
   }
 
 }
