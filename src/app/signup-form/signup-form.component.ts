56fc9203437150806fb0ae828f8f3affa855bfce
Magico80 <magie.cannizzaro@gmail.com>
2018-01-02 12:00:28 +0100

    Adding validation by code NOT html

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {FormGroup,FormControl} from '@angular/forms'
+import {FormGroup,FormControl,Validators} from '@angular/forms'
+
 @Component({
   selector: 'signup-form',
   templateUrl: './signup-form.component.html',
@@ -7,8 +8,12 @@ import {FormGroup,FormControl} from '@angular/forms'
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl(),
-    'password': new FormControl()
+    username: new FormControl('nome',Validators.required,), //static method of validators, called without ()
+    'password': new FormControl('',Validators.required)
     
   });
+
+  get password(){
+    return this.form.get('password');
+  }
 }
