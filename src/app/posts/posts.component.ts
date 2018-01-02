3bfdf46cc96e726d9232a053bb70a74bf010934b
Magico80 <magie.cannizzaro@gmail.com>
2018-01-02 17:09:10 +0100

    Update Data - put vs patch

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -33,4 +33,19 @@ export class PostsComponent implements OnInit {
       })
   }
 
+  updatePost(post){
+    //.patch => update only few properties
+    this.http.patch(this.url+'/'+post.id,JSON.stringify({isRead:true}))
+      .subscribe(response=>{
+        console.log(response.json())
+      })
+
+    //.put => update all the obejct
+    /*this.http.put(this.url,JSON.stringify(post))
+      .subscribe(response=>{
+        console.log(response.json())
+      })
+      */
+  }
+
 }
