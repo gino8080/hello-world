423559b643ea6ab0b654a90b8c1f115ea9284fa6
Magico80 <magie.cannizzaro@gmail.com>
2018-01-19 14:18:04 +0100

    Programmatic Navigation

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,28 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent implements OnInit {
+export class GithubProfileComponent{
 
   //ActivatedRoute to get paramters
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:Router) { }
 
-  ngOnInit() {
-    //everytime the params change over time we are notified
-    //everytime we change page the component is destroyed 
-   console.log("GithubProfileComponent ON INIT")
-   //on the same page we can observe the paramMap for the changing paramas
-   this.route.paramMap.subscribe(params=>{
-     console.info(params.get("id"))
-   })
-
-   //usefull when reinitialized and if we don't have the NEXT/PREV button or navigate AWAY with back button
-   let snapShotID = this.route.snapshot.paramMap.get("id")
-   console.info("snapShotID",snapShotID)
+  submit(){
+    this.route.navigate(['/followers'],{
+      queryParams : { page:1, order:'newest' }
+    })
   }
 
 }
