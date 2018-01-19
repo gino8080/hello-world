3b56f55e9c1cd471275955aac8c9c2713d4fa277
Magico80 <magie.cannizzaro@gmail.com>
2018-01-19 14:01:30 +0100

    ROUTING - combine observables required and optional

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,6 +1,8 @@
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router/';
+import {Observable} from "rxjs/Observable"
+import "rxjs/add/observable/combineLatest" //method to combine multiple observables
 
 @Component({
   selector: 'github-followers',
@@ -16,19 +18,23 @@ export class GithubFollowersComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    //required params 2 methods observable or snapshot
-    this.route.paramMap.subscribe(requiredParams=>{
 
-    });
-    let id = this.route.snapshot.paramMap.get("id") //required params
+    //combine multiple observable into 1
+    let obs = Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    obs.subscribe(combined=>{
+      //required paramMap
+      let id = combined[0].get("id");
+      let page = combined[1].get("page");
 
-    //optional params 2 methods observable or snapshot
-    this.route.queryParamMap.subscribe(optionalParams=>{
+      console.log(id, page)
+      //this.service.getAll({id:id, page:page}) //our getAll does not accept params, but this is the way!
+      this.service.getAll()
+        .subscribe(followers => this.followers = followers);
+    })
 
-    });
-    let page = this.route.snapshot.queryParamMap.get("page");
-
-    this.service.getAll()
-      .subscribe(followers => this.followers = followers);
+   
   }
 }
